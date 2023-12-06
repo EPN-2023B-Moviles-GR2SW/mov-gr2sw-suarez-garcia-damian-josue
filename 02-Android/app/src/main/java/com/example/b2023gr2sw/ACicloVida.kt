994cad00896c11c29629827841f5b214c879662c
D4M1GOD <damian20002010@live.com>
2023-12-06 09:42:14 -0500

    new: Activity Running

--- 02-Android/app/src/main/java/com/example/b2023gr2sw/ACicloVida.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/ACicloVida.kt
@@ -2,10 +2,69 @@ package com.example.b2023gr2sw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 
 class ACicloVida : AppCompatActivity() {
+    var textoGlobal= ""
+    fun mostrarSnackbar(texto:String){
+        textoGlobal += texto
+        val snack = Snackbar.make(findViewById(R.id.cl_ciclo_vida),
+            textoGlobal, Snackbar.LENGTH_LONG)
+        snack.show()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aciclo_vida)
+        mostrarSnackbar("Hola")
+        mostrarSnackbar("OnCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mostrarSnackbar("OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mostrarSnackbar("OnResumen")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        mostrarSnackbar("OnRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mostrarSnackbar("OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mostrarSnackbar("OnStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mostrarSnackbar("OnDestroy")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        //RECUPERAR LAS VARIABLES
+        val textoRecuperado:String? = savedInstanceState
+            .getString("TextoGuardado")
+
+        if(textoRecuperado != null){
+            mostrarSnackbar(textoRecuperado)
+            textoGlobal = textoRecuperado
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run {
+            putString("TextoGuardado",textoGlobal)
+        }
+        super.onSaveInstanceState(outState)
     }
 }
